a4dbd2d63279196e35363aac82a5e425b55fb247
Victor Johnson <victorjohnson@victors-mbp.attlocal.net>
2018-05-25 02:44:00 -0500

    Initial Commit

---
+++ nightshift/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nightshift
+//
+//  Created by Victor Johnson on 5/25/18.
+//  Copyright © 2018 Victor Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
